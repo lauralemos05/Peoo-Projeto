733dbe3606377eb29dbb88734971b355442ffe12
eduardasouz <122638796+eduardaaugusta@users.noreply.github.com>
2023-12-25 12:20:52 -0300

    Update maquiagem.py

--- maquiagem.py
+++ maquiagem.py
@@ -1,11 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class Cliente:
-    def __init__(self, nome, horario, dia):
-        self.nome = nome
-        self.horario = horario
-        self.dia = dia
+
 
 class MaquiadoraApp:
     def __init__(self, master):
