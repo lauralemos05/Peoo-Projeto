0bb09c7853674934ef9149b17f039dcfcc826f31
ElyssonHiago <99830595+ElyssonHiago@users.noreply.github.com>
2023-12-25 23:53:37 -0300

    .

--- maquiagem.py
+++ maquiagem.py
@@ -5,7 +5,7 @@ class Cliente:
     def __init__(self, nome, horario, dia):
         self.nome = nome
         self.horario = horario
-        self.di = dia
+        self.dia = dia
 
 class MaquiadoraApp:
     def __init__(self, master):
